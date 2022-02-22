4e03a82df6479c2a51cbc09673b749da050b26b3
Mateus Silva Freitas <mateus.freitas@paytrack.com.br>
2022-02-21 21:51:10 -0300

    Commit

---
+++ src/main/java/com/rabbitmq/springrabbitmqconsumer/SpringRabbitmqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.springrabbitmqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitmqConsumerApplication.class, args);
+	}
+
+}
